c889ab42d1eac1640301b71768ecb6e6985750e1
BBeerBear <xiongx659@gmail.com>
2023-07-22 11:07:07 -0400

    add labi and tv

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
+import Firework from 'react-firework';
+import Confetti from 'react-confetti';
+import ReactPlayer from 'react-player/youtube';
+import labicake from './assets/labicake.png';
+
 function App() {
   return (
-    <div className='bg-black/25 w-screen h-screen p-10 flex flex-col gap-6 '>
-      <div className='font-zcool text-6xl tracking-wider text-sky-400 hover:text-7xl transition-all duration-1000'>
-        祝熊玉琛生日快樂！
-      </div>
-      <div className='font-dancing text-6xl tracking-wider text-pink-400 hover:text-7xl transition-all duration-1000'>
+    <div className='relative bg-white/25 w-screen h-screen p-2 flex flex-col items-center justify-center gap-5 overflow-hidden'>
+      <div className='font-dancing text-6xl tracking-wider text-pink-400 hover:text-7xl duration-1000 z-50 w-fit '>
         Happy Birthday!
       </div>
-      <div>
-        {/* <ReactAudioPlayer src='./happy_birthday.mp3' autoPlay loop /> */}
+      <div className='font-zcool text-6xl tracking-wider text-sky-400 hover:text-7xl duration-1000 z-50 w-fit'>
+        🎉祝熊玉琛生日快樂！🎁
+      </div>
+
+      <div className='relative bg-mytv bg-cover h-80 w-96 shadow-lg hover:scale-105 duration-1000'>
+        {/* // Only loads the YouTube player */}
+        <div
+          className='absolute inset-0' // Full width and height of parent
+          style={{
+            objectFit: 'cover', // Resize to fill parent
+            objectPosition: 'center center', // Center the video
+            // Adjust these values to fit the transparent area in the TV
+            top: '10%', // Move down
+            left: '10%', // Move right
+            width: '80%', // Adjust width
+            height: '70%', // Adjust height
+          }}
+        >
+          <ReactPlayer
+            url='https://www.youtube.com/watch?v=ZEyXRST5XdM'
+            loop={true}
+            width={250}
+            height={190}
+          />
+        </div>
+      </div>
+      <div className='absolute bottom-2 w-screen animate-move'>
+        <img src={labicake} className='brightness-100 w-60 ' />
+      </div>
+      <div className='-z-50'>
+        <Confetti />
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,25 @@ export default {
         dancing: ['Dancing Script', 'cursive'],
       },
       backgroundImage: {
-        mybg: "url('/src/assets/bg.jpg')",
+        mybg: "url('/src/assets/bg2.jpg')",
+        mytv: "url('/src/assets/oldtv.png')",
+      },
+      keyframes: {
+        move: {
+          '0%': { transform: 'translateX(100%) translateY(0%)' },
+          '10%': { transform: 'translateX(90%) translateY(20%)' },
+          '20%': { transform: 'translateX(80%) translateY(0%)' },
+          '40%': { transform: 'translateX(60%) translateY(20%)' }, // 第一次向上跳跃
+          '50%': { transform: 'translateX(50%) translateY(0)' }, // 回到原地
+          '60%': { transform: 'translateX(40%) translateY(20%)' }, // 第二次向上跳跃
+          '70%': { transform: 'translateX(30%) translateY(0)' }, // 回到原地
+          '80%': { transform: 'translateX(15%) translateY(20%)' },
+          '90%': { transform: 'translateX(0%) translateY(0)' },
+          '100%': { transform: 'translateX(-20%) translateY(20%)' },
+        },
+      },
+      animation: {
+        move: 'move 10s infinite linear',
       },
     },
   },
